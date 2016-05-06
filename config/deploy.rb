b650aeb070c2f1286b178dd791799fda362a48b9
Denis S. Runov <it-saratov@mail.ru>
2016-05-06 13:47:36 +0300

    bundle

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :git
 set :deploy_to, '/home/deployer/qna'
 set :deploy_user, 'deployer'
 set :bundle_path, '/home/deployer/qna/shared/bundle'
-set :rvm_type, :auto
+set :rvm_type, :system
 
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
