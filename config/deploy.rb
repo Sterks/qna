8a70d87b8ce956a7769bfbbcb4a0f218c5fbae1d
Denis S. Runov <it-saratov@mail.ru>
2016-05-06 10:18:18 +0300

    cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :format_options, command_output: true, log_file: 'log/capistrano.log', color
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/private_pub.yml', '.env')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
