e65d3f3f8d38a91550a3e35ab8507ef9005d7cc5
Denis S. Runov <it-saratov@mail.ru>
2016-05-06 12:14:48 +0300

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :questions
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'questions#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
