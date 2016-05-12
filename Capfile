48f3cc706e3b1242774bc36e5b7fcbcf63e041a5
Denis S. Runov <it-saratov@mail.ru>
2016-05-12 09:44:29 +0300

    Add unicorn

--- Capfile
+++ Capfile
@@ -5,7 +5,7 @@ require "capistrano/deploy"
 require 'capistrano/rails'
 require 'capistrano/bundler'
 require 'capistrano/rvm'
-
+require 'capistrano3/unicorn'
 # Include tasks from other gems included in your Gemfile
 #
 # For documentation on these, see for example:
--- config/deploy.rb
+++ config/deploy.rb
@@ -25,16 +25,14 @@ set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/ca
 set :keep_releases, 5
 
 namespace :deploy do
-
-
     desc 'Restart application'
     task :restart do
       on roles(:app), in: :sequence, wait: 5 do
-        execute :touch, release_path.join('tmp/restart.txt')
+        #execute :touch, release_path.join('tmp/restart.txt')
+        invoke 'unicorn:restart'
       end
     end
 
     after :publishing, :restart
-
 end
 
